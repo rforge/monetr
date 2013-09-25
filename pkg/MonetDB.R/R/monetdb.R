d0eb36dfb8decc33c247f55f7964bff001499f13
hannes <hannes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-25 11:12:35 +0000

    mc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/monetr@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MonetDB.R/R/monetdb.R
+++ pkg/MonetDB.R/R/monetdb.R
@@ -569,7 +569,7 @@ REPLY_SIZE    <- 100 # Apparently, -1 means unlimited, but we will start with a 
 
 .mapiCleanup <- function(conObj) {
 	if (conObj@connenv$lock > 0) {
-		if (DEBUG) cat("II: Interrupted query execution.\n")
+		if (DEBUG_QUERY) cat("II: Interrupted query execution.\n")
 		conObj@connenv$lock <- 0
 	}
 }
